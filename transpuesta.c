f1334e4b82a793d791864f775eedf785b69c7b05
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-11-27 16:32:29 -0500

    Ingreso de lineas para definir el tamaño de la matriz por teclado

---
+++ transpuesta.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define max 100
+ //La matriz debe contener numeros entre el 0 y el 100
+int main(int argc, char const *argv[])
+{
+    int Matriz[max][max];
+    int f, c;
+
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &f);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &c);
+
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("Ingrese los valores de la matriz [%d][%d]: ",i+1, j+1);
+            scanf("%d\n", &Matriz[i][j]);
+        }
+        
+    } 
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d\t", Matriz[i][j]);
+        }
+        printf("\n");
+    }
+   
+    
+
+    return 0;
+}
